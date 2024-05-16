145bdf48a8a8fd14d33573bc08141b9e6cf8b370
BiancaDullius <bianca.dullius@paipe.co>
2024-05-16 02:49:58 -0300

    feat: update tech stack

---
+++ README.md
@@ -0,0 +1,1 @@
+http:\\biancabeppler.com.br
--- script.js
+++ script.js
@@ -32,3 +32,38 @@ let typing = setInterval(() => {
         i++;
     }
 }, 100);
+
+
+
+
+
+
+
+
+// Função para verificar se um elemento está visível na tela
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Função para adicionar a classe de animação quando o elemento estiver visível
+function addAnimationClass() {
+    var elements = document.querySelectorAll('.cards_item');
+    elements.forEach(function (el) {
+        if (isElementInViewport(el)) {
+            el.style.opacity = "1";
+            el.classList.add('slideInUp');
+        }
+    });
+}
+
+// Adiciona o evento de rolagem da página para verificar quando os elementos estão visíveis
+window.addEventListener('scroll', addAnimationClass);
+
+// Chama a função uma vez ao carregar a página para verificar os elementos iniciais
+window.addEventListener('load', addAnimationClass);
